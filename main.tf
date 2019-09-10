edd57d1412fd83ef9d4cec2648f338bb1722b7b4
Ryan Kelley <rykelley@MacBook-Pro.localdomain>
2019-09-10 11:17:25 -0600

    fixed bug in heredoc

--- main.tf
+++ main.tf
@@ -1,9 +1,32 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_security_group" "instance" {
+  name = "tf-webserver-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
+
 resource "aws_instance" "single" {
-  ami           = "ami-00c03f7f7f2ec15c3"
-  instance_type = "t2.micro"
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
 
   tags = {
