4f8cf83243ff4132f741425ceb6397fa0ee2f762
Ryan Kelley <rkelley@skylandanalytics.com>
2019-09-12 10:35:05 -0600

    removed workspaces demo

--- stage/data-storage/mysql/main.tf
+++ stage/data-storage/mysql/main.tf
@@ -12,6 +12,7 @@ resource "aws_db_instance" "example" {
 
   password            = "password1234" #data.aws_secretsmanager_secret_version.db_password.secret_string
   skip_final_snapshot = true
+# storage_encrypted = true  
 }
 
 #data "aws_secretsmanager_secret_version" "db_password" {
