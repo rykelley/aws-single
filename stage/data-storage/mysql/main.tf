1bca2f89af8f259a5d29be9e38a885a87fffccf3
Ryan Kelley <rykelley@MacBook-Pro.localdomain>
2019-09-11 19:47:53 -0600

    stuck on password

--- stage/data-storage/mysql/main.tf
+++ stage/data-storage/mysql/main.tf
@@ -10,9 +10,9 @@ resource "aws_db_instance" "example" {
   name              = "test_database"
   username          = "admin"
 
-  password = data.aws_secretsmanager_secret_version.db_password.secret_string
+  password = "password1234" #data.aws_secretsmanager_secret_version.db_password.secret_string
 }
 
-data "aws_secretsmanager_secret_version" "db_password" {
-  secret_id = "mysql-master-password-stage"
-}
+#data "aws_secretsmanager_secret_version" "db_password" {
+#  secret_id = "mysql-master-password-stage"
+#}
